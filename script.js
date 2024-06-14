000a52750f0bb31c493b8c9ef314fd8f0d5c28af
Samuel Ramos <samuelvanguarda@gmail.com>
2024-06-14 16:03:17 -0300

    FEAT: recover DOM elements

---
+++ script.js
@@ -0,0 +1,5 @@
+const nameInput = document.getElementById('name');
+const heightInput = document.getElementById('height');
+const weightInput = document.getElementById('weight');
+const calculateBtn = document.getElementById('calculate');
+const resultDiv = document.getElementById('result');
